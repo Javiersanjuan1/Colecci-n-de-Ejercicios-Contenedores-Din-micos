1e286f1d38e6c973af62bdf4096653ada4a7fd65
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-30 11:17:48 +0100

    Initial commit

--- Vector e Iteradores.cpp
+++ Vector e Iteradores.cpp
@@ -5,7 +5,7 @@ int main() {
     // Crear un vector de entero
     std::vector<int> miVector;
 
-    // Añadir elementos al vector utilizando pus
+    // Añadir elementos al vector utilizando
     miVector.push_back(1);
     miVector.push_back(2);
     miVector.push_back(3);
